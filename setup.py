45bd0fa49ddeb9f936c9f68a6b617eee7482e4f8
Namlook <n.namlook@gmail.com>
2012-09-04 12:13:08 +0200

    bump v1.4

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ import codecs
 
 setup(
     name = 'MongoLite',
-    version = '1.3', # TODO don't forget to change version in __init__.py
+    version = '1.4', # TODO don't forget to change version in __init__.py
 
     description = 'Python MongoDB light toolkit',
     long_description = codecs.open('README.rst', "r", "utf-8").read(),
